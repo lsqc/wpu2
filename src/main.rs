70efdb28e9c2d9af6eb551582b0e513221b51550
lsqc <lsqc@nya.vodka>
2026-01-12 10:38:48 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::fs::File;
+use std::io;
+
+use std::path;
+use std::path::PathBuf;
+use std::process;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let mut url: String = String::new();
+    let mut dest: String = String::new();
+
+    if args.len() > 2 {
+        // great, we've got enough arguments
+        url = args.get(1).expect("something fucky happened [1]").to_string();
+        dest = args.get(2).expect("something fucky happened [2]").to_string();
+    }
+    else {
+        println!("usage: {} <url> <dest>", args.get(0).unwrap().to_string());
+        process::exit(0x1);
+        
+    }
+
+    let path_buf: PathBuf = PathBuf::from(&dest);
+    let absolute_dest = path::absolute(&path_buf).expect("could not create absolute path");
+    let absolute_path = absolute_dest.display();
+
+    println!("debug: absolute path: {}", &absolute_path);
+    println!("saving {} to {}", url, &absolute_path);
+
+    let resp = reqwest::get(url.to_string())
+        .await
+        .expect("could not complete request");
+
+    let body = resp.text().await.expect("response body invalid");
+    let mut out = File::create(&dest).expect("could not create file");
+
+    io::copy(&mut body.as_bytes(), &mut out).expect("failed to copy content");
+}
